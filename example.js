9a019c2c9b0cfb1c255f5d553ed58fcc51bab09d
Felipe Garcia Diaz <41710468+felipeGarciaDiaz@users.noreply.github.com>
2021-04-19 02:41:20 -0400

    Update example.js

--- example.js
+++ example.js
@@ -3,20 +3,20 @@ const regforms = require("./regex-forms.js");
 console.log(
 	"These should be true:\n",
   
-  regforms.validator.title("This is an example of a title! $100.00"),
+  regforms.validator.title("This is an example of a title! $100.00", 100, 10),
 	regforms.validator.email("myemail@gmail.com"),
 	regforms.validator.price("$50,000.00"),
 	regforms.validator.tags("#test, #test2"),
 	regforms.validator.phone("8139924795"),
-	regforms.validator.description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation")
+	regforms.validator.description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation", 1500, 100)
 );
 console.log(
 	"These should be false:\n",
   
-  regforms.validator.title("no title"),
+  regforms.validator.title("no title", 100, 10),
 	regforms.validator.email("hey! this is not an email"),
 	regforms.validator.price("thisisnotanumber"),
 	regforms.validator.tags("nota#lol"),
 	regforms.validator.phone("323.rwwa.35tate"),
-	regforms.validator.description("tryme:)")
+	regforms.validator.description("tryme:)", 1500, 100)
 );
